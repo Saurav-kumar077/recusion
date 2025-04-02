14a1015cc4a43bd90afe7ce83e10f049dc6a1828
Saurav-kumar077 <126255878+Saurav-kumar077@users.noreply.github.com>
2025-04-02 09:18:13 +0530

    updated

--- count.cpp
+++ count.cpp
@@ -2,18 +2,15 @@
 using namespace std;
 void print(int n){
     if(n==0){
-        return ;
+        return;
     }
-    
-    print(n-1);
-    cout << n <<endl;
-}
-
+print(n-1);
+cout << n <<endl;
+ }
 int main(){
-    int n;
-    cout<<"Enter the number "<<endl;
-    cin>>n;
-    
-   print(n-1);
-    return 0;
+int n;
+cout<<"Enter the number "<<endl;
+cin>>n;   
+print(n);
+ return 0;
 }
